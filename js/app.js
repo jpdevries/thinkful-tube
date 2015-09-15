51337a0f2eabb7fe4d4d8648c10bd7a7d93c3f40
JP DeVries <johnpaul.devries@gmail.com>
2015-09-15 10:25:56 -0700

    Patching API Endpoint

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function getRequest(searchTerm){
 		key: 'AIzaSyCX18fh2GihWojMUnf2jjw_19xESuYytDA',
 		dataType: 'jsonp'
 	};
-	url = 'https://www.youtube.com';
+	url = 'https://www.googleapis.com/youtube/v3/search/';
 
 	$.getJSON(url, params, function(data){
 		console.log(data);
